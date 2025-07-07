a63f67901e786879057bfc86631fbf8db75ce5df
Leonardo Silva <leo.bs1997@hotmail.com>
2025-07-07 20:50:47 -0300

    arquivos alterados

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from 'express';
 import publicRoutes from './routes/public.js';
 import privateRoutes from './routes/private.js';
 import auth from './middlewares/auth.js';
+import cors from 'cors';
 
 const PORT = parseInt(process.env.PORT, 10);
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/', publicRoutes);
 
